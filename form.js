44fae31f799a43ae909ccd737a3ed2dd7ebce7a9
PNHJunSiang <137973383+PNHJunSiang@users.noreply.github.com>
2023-07-05 16:41:11 +0800

    completed contact page

---
+++ form.js
@@ -0,0 +1,18 @@
+
+
+function ajaxpost () {
+    // (A) GET FORM DATA
+    var data = new FormData(document.getElementById("myForm"));
+   
+    // (B) AJAX REQUEST
+    // (B1) POST DATA TO SERVER, RETURN RESPONSE AS TEXT
+    fetch("1c-server.html", { method:"POST", body:data })
+    .then(res => res.text())
+   
+    // (B2) SHOW MESSAGE ON SERVER RESPONSE
+    .then(response => {
+      console.log(response);
+      if (response == "OK") { alert("SUCCESSFUL!"); }
+      else { alert("FAILURE!"); }
+    } )
+}
